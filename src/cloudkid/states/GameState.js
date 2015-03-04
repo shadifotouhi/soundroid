3e4f6ac225b1b23f0da21858e457a1a4b9e78db7
Shadi Fotouhi <shadifotouhi@shadi.cloudkid.net.cloudkid.net>
2015-03-04 13:26:40 -0500

    all sounds play

--- src/cloudkid/states/GameState.js
+++ src/cloudkid/states/GameState.js
@@ -2,7 +2,8 @@
 {
     //Import classes
     var BaseState = include('pbskids.createjs.BaseState'),
-        SpriteUtils = include('pbskids.createjs.SpriteUtils');
+        SpriteUtils = include('pbskids.createjs.SpriteUtils'),
+        Sound = include('springroll.Sound');
 
     /**
      * Saved access to the Progress Tracker
@@ -18,7 +19,10 @@
     var GameState = function()
     {
         BaseState.call(this, new cloudkid.GamePanel(), 'title', 'title');
-        this._onPlayHelp = this._onPlayHelp.bind(this);
+
+
+        //list of all of the sounds
+        this.sounds = [];
     };
 
     //Super prototype
@@ -28,58 +32,25 @@
     var p = extend(GameState, BaseState);
 
     /**
-     * Add custom tasks to the list of preload tasks
-     * @param {array} tasks The list of task to preload
-     */
-
-
-    p.addTasks = function(tasks)
-    {
-        SpriteUtils.addSpritesRequest(
-            this.config.spriteManifests.game,
-            this.panel,
-            tasks,
-            'assets/images/sprites/');
-    };
-
-    /**
      * After assets are loaded and state is fully entered
      */
 
 
-    /*p.enterDone = function()
+    p.enterDone = function()
     {
-        this.game.messenger.on('playHelp', this._onPlayHelp);
-        _tracker = this.game.tracker;
+
+        for(var key in Sound.instance._sounds)
+        {
+            this.sounds.push(key);
+        }
+
+        this.game.player.play(
+            this.sounds
+            );
+        
     };
 
-    /**
-     * Listens for playHelp message fired by the Help button
-     * on the Kraken site/PBS app.
-     */
 
-
-    p._onPlayHelp = function()
-    {
-        // this.game.tracker.clickHelp(
-        //     game_specific_tracking_paramter_1,
-        //     game_specific_tracking_paramter_2
-        // );
-    };
-
-    /**
-     *
-     */
-
-
-    p.exitStart = function()
-    {
-        this.game.messenger.off('playHelp', this._onPlayHelp);
-    };
-
-    /**
-     * When the state exists
-     */
     p.exit = function()
     {
         s.exit.call(this);
